945bc7a6fb6632730bf6c8d135be8d2dac3ae4cc
Quốc Chương <quocchuong2609@gmail.com>
2025-10-05 10:23:34 +0700

    refactor: add return types to resolver methods for better type safety

--- src/users/resolvers/users.resolver.ts
+++ src/users/resolvers/users.resolver.ts
@@ -19,42 +19,46 @@ export class UsersResolver {
 	constructor(private readonly usersService: UsersService) {}
 
 	@Mutation(() => User)
-	async createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
-		return this.usersService.create(createUserInput);
+	async createUser(
+		@Args('createUserInput') createUserInput: CreateUserInput,
+	): Promise<any> {
+		return await this.usersService.create(createUserInput);
 	}
 
 	@Query(() => [User], { name: 'users' })
-	async findAll() {
-		return this.usersService.findAll();
+	async findAll(): Promise<any[]> {
+		return await this.usersService.findAll();
 	}
 
 	@Query(() => User, { name: 'user' })
-	async findOne(@Args('id', { type: () => Int }) id: number) {
-		return this.usersService.findOne(id);
+	async findOne(@Args('id', { type: () => Int }) id: number): Promise<any> {
+		return await this.usersService.findOne(id);
 	}
 
 	@Query(() => User, { name: 'userByEmail' })
-	async findByEmail(@Args('email') email: string) {
-		return this.usersService.findByEmail(email);
+	async findByEmail(@Args('email') email: string): Promise<any> {
+		return await this.usersService.findByEmail(email);
 	}
 
 	@Mutation(() => User)
-	async updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
-		return this.usersService.update(updateUserInput.id, updateUserInput);
+	async updateUser(
+		@Args('updateUserInput') updateUserInput: UpdateUserInput,
+	): Promise<any> {
+		return await this.usersService.update(updateUserInput.id, updateUserInput);
 	}
 
 	@Mutation(() => User)
-	async removeUser(@Args('id', { type: () => Int }) id: number) {
-		return this.usersService.remove(id);
+	async removeUser(@Args('id', { type: () => Int }) id: number): Promise<any> {
+		return await this.usersService.remove(id);
 	}
 
 	@ResolveField('restaurants', () => [Restaurant])
-	getRestaurants(@Parent() user: User) {
+	getRestaurants(@Parent() user: User): Promise<any[]> {
 		return this.usersService.findRestaurantsByOwner(user.id);
 	}
 
 	@ResolveField('orders', () => [Order])
-	getOrders(@Parent() user: User) {
+	getOrders(@Parent() user: User): Promise<any[]> {
 		return this.usersService.findOrdersByUser(user.id);
 	}
 }
