d5f3ae5558cc87608612403307eed117f866c3a8
Quốc Chương <quocchuong2609@gmail.com>
2025-10-05 09:57:59 +0700

    add: create AppModule to initialize application with necessary modules and GraphQL configuration

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+import { MenuItemsModule } from '@/menu-items/menu-items.module';
+import { OrdersModule } from '@/orders/orders.module';
+import { PrismaModule } from '@/providers/prisma/prisma.module';
+import { RestaurantsModule } from '@/restaurants/restaurants.module';
+import { UsersModule } from '@/users/users.module';
+
+@Module({
+	imports: [
+		GraphQLModule.forRoot<ApolloDriverConfig>({
+			driver: ApolloDriver,
+			autoSchemaFile: true, // Tự động tạo schema từ decorators
+			playground: true, // Bật GraphQL Playground
+			introspection: true, // Cho phép introspection
+		}),
+		UsersModule,
+		RestaurantsModule,
+		MenuItemsModule,
+		OrdersModule,
+		PrismaModule,
+	],
+})
+export class AppModule {}
