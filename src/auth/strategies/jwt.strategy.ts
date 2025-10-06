8dc2674e20e43464db6fc9930d704e1b7e9a001b
Quốc Chương <quocchuong2609@gmail.com>
2025-10-06 16:12:57 +0700

    feat: add JWT strategy for authentication using Passport

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { UsersService } from '@/users/services/users.service';
+
+export interface JwtPayload {
+	sub: number;
+	email: string;
+	role: string;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+	constructor(private usersService: UsersService) {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			ignoreExpiration: false,
+			secretOrKey: process.env.JWT_SECRET || 'bakaqc',
+		});
+	}
+
+	async validate(payload: JwtPayload) {
+		const user = await this.usersService.findOne(payload.sub);
+		return user;
+	}
+}
