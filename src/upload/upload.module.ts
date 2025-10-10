512765f888c59dbb53c6be52889ffca3d8704e95
Quốc Chương <quocchuong2609@gmail.com>
2025-10-10 09:26:17 +0700

    feat: add UploadController and UploadScalar to UploadModule

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -3,12 +3,15 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '@/auth/auth.module';
 import { UsersModule } from '@/users/users.module';
 
+import { UploadController } from './upload.controller';
 import { UploadResolver } from './upload.resolver';
+import { UploadScalar } from './upload.scalar';
 import { UploadService } from './upload.service';
 
 @Module({
 	imports: [AuthModule, UsersModule],
-	providers: [UploadService, UploadResolver],
+	controllers: [UploadController],
+	providers: [UploadService, UploadResolver, UploadScalar],
 	exports: [UploadService],
 })
 export class UploadModule {}
