f8c2f438d5ec6633d3d8562420f7d143c0d626c3
Quốc Chương <quocchuong2609@gmail.com>
2025-10-19 16:40:23 +0700

    refactor: remove UploadResolver from UploadModule providers and include CloudinaryService

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -3,15 +3,16 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '@/auth/auth.module';
 import { UsersModule } from '@/users/users.module';
 
+import { CloudinaryService } from './cloudinary.service';
 import { UploadController } from './upload.controller';
-import { UploadResolver } from './upload.resolver';
+// import { UploadResolver } from './upload.resolver'; // Temporarily disabled
 import { UploadScalar } from './upload.scalar';
 import { UploadService } from './upload.service';
 
 @Module({
 	imports: [AuthModule, UsersModule],
 	controllers: [UploadController],
-	providers: [UploadService, UploadResolver, UploadScalar],
+	providers: [UploadService, UploadScalar, CloudinaryService], // Removed UploadResolver temporarily
 	exports: [UploadService],
 })
 export class UploadModule {}
